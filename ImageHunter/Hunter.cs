97225843a2b36a96f14edd40c28b02807bd3b46e
Fran Hoey <fran@bluejumper.com>
2015-06-25 17:38:20 +0100

    Initial Commit

---
+++ ImageHunter/Hunter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ImageHunter
+{
+    public class Hunter
+    {
+        public string SearchFileExtensions { get; set; }
+        public string SearchPath { get; set; }
+
+        public void Run()
+        {
+            HuntFolder(SearchPath);
+        }
+
+        private void HuntFolder(string path)
+        {
+            if (!Directory.Exists(path))
+                return;
+
+            var files = Directory.GetFiles(path, SearchFileExtensions);
+            foreach (var file in files)
+                HuntFile(file);
+
+            var subDirectories = Directory.GetDirectories(path);
+            foreach (var subDirectory in subDirectories)
+            {
+                HuntFolder(subDirectory);
+            }
+        }
+
+        private void HuntFile(string path)
+        {
+            if (!File.Exists(path))
+                return;
+
+            Console.WriteLine(path);
+        }
+    }
+}
---
+++ ImageHunter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageHunter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hunter = new Hunter()
+            {
+                SearchPath = @"C:\Projects\Yara\yara-com\src\Web\BB.Yara.Com\BB.Yara.Web.Com",
+                SearchFileExtensions = "*.aspx"
+            };
+            hunter.Run();
+
+            Console.WriteLine();
+            Console.WriteLine("Finished");
+            Console.ReadKey();
+        }
+    }
+}
