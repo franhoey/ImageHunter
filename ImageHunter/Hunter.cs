6c2efc857660e9e405dbc9d79a802f88fc0e8177
Fran Hoey <fran@bluejumper.com>
2015-06-26 11:07:09 +0100

    Added TPL and parallelism

--- ImageHunter/Hunter.cs
+++ ImageHunter/Hunter.cs
@@ -1,40 +1,123 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
 
 namespace ImageHunter
 {
-    public class Hunter
+    public class Hunter : IDisposable
     {
+        private const string LOG_FILE_NAME = "output.log";
+
+        private readonly TransformManyBlock<string, string> _findFilesBlock;
+        private readonly TransformManyBlock<string, string> _findImagesBlock;
+        private readonly ActionBlock<string> _outputImagesBlock;
+
+        private StreamWriter _logWriter;
+        private bool _logIsOpen;
+
         public string SearchFileExtensions { get; set; }
         public string SearchPath { get; set; }
-
-        public void Run()
+        public int MaxDegreeOfParallelism { get; private set; }
+       
+        public Hunter() : this(1)
         {
-            HuntFolder(SearchPath);
         }
 
-        private void HuntFolder(string path)
+        public Hunter(int maxDegreeOfParallelism)
         {
-            if (!Directory.Exists(path))
-                return;
+            MaxDegreeOfParallelism = maxDegreeOfParallelism;
+            _logIsOpen = false;
+            _findFilesBlock = new TransformManyBlock<string, string>((Func<string, IEnumerable<string>>)FindFiles);
+            _findImagesBlock = new TransformManyBlock<string, string>((Func<string, IEnumerable<string>>)FindImagesInFile, new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = maxDegreeOfParallelism });
+            _outputImagesBlock = new ActionBlock<string>((Action<string>) OutputImages);
+            BuildTplNetwork();
+        }
 
-            var files = Directory.GetFiles(path, SearchFileExtensions);
+        private IEnumerable<string> FindFiles(string folderPath)
+        {
+            var files = Directory.GetFiles(folderPath, SearchFileExtensions);
             foreach (var file in files)
-                HuntFile(file);
-
-            var subDirectories = Directory.GetDirectories(path);
+                yield return file;
+            
+            var subDirectories = Directory.GetDirectories(folderPath);
             foreach (var subDirectory in subDirectories)
             {
-                HuntFolder(subDirectory);
+                foreach (var file in FindFiles(subDirectory))
+                {
+                    yield return file;
+                }
             }
         }
 
-        private void HuntFile(string path)
+        private IEnumerable<string> FindImagesInFile(string filePath)
         {
-            if (!File.Exists(path))
-                return;
+            Thread.Sleep(1000);
+            return filePath.Split('\\');
+        }
 
-            Console.WriteLine(path);
+        private void OutputImages(string imagePath)
+        {
+            if(imagePath.EndsWith(".aspx"))
+                _logWriter.WriteLine(imagePath);
+        }
+
+        public void Run()
+        {
+            OpenLogFile();
+
+            _findFilesBlock.Post(SearchPath);
+            _findFilesBlock.Complete();
+            _outputImagesBlock.Completion.Wait();
+
+            CloseLogFile();
+        }
+
+        private void BuildTplNetwork()
+        {
+            _findFilesBlock.LinkTo(_findImagesBlock);
+            _findImagesBlock.LinkTo(_outputImagesBlock);
+
+            _findFilesBlock.Completion.ContinueWith(t =>
+            {
+                if (t.IsFaulted) ((IDataflowBlock)_findImagesBlock).Fault(t.Exception);
+                else _findImagesBlock.Complete();
+            });
+            _findImagesBlock.Completion.ContinueWith(t =>
+            {
+                if (t.IsFaulted) ((IDataflowBlock) _outputImagesBlock).Fault(t.Exception);
+                else _outputImagesBlock.Complete();
+            });
+        }
+
+        private void OpenLogFile()
+        {
+            if (_logIsOpen)
+                return;
+            
+            if(File.Exists(LOG_FILE_NAME))
+                File.Delete(LOG_FILE_NAME);
+
+            _logWriter = File.CreateText(LOG_FILE_NAME);
+
+            _logIsOpen = true;
+        }
+
+        private void CloseLogFile()
+        {
+            if (_logIsOpen)
+            {
+                _logWriter.Flush();
+                _logWriter.Close();
+                _logIsOpen = false;
+            }
+        }
+
+        public void Dispose()
+        {
+            CloseLogFile();
         }
     }
 }
--- ImageHunter/Program.cs
+++ ImageHunter/Program.cs
@@ -1,25 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace ImageHunter
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            var hunter = new Hunter()
+            var processorCount = Environment.ProcessorCount;
+            var maxDegreeOfParallelism = processorCount * 4;
+            Console.WriteLine("processorCount: {0}", processorCount);
+            Console.WriteLine("maxDegreeOfParallelism:{0}", maxDegreeOfParallelism);
+            Console.WriteLine();
+
+            var hunter = new Hunter(maxDegreeOfParallelism)
             {
                 SearchPath = @"C:\Projects\Yara\yara-com\src\Web\BB.Yara.Com\BB.Yara.Web.Com",
                 SearchFileExtensions = "*.aspx"
             };
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             hunter.Run();
 
+            stopwatch.Stop();
+
             Console.WriteLine();
+            Console.WriteLine("Time elapsed:{0}", stopwatch.ElapsedMilliseconds);
             Console.WriteLine("Finished");
             Console.ReadKey();
         }
     }
 }
+    
